fb972e10f43d3fe83691a3afeb18c1737f68c0f1
abhishekhbihari007 <128975284+abhishekhbihari007@users.noreply.github.com>
2025-07-07 15:29:39 +0000

    #i have learn conversition operations

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS as newer version
+console.log(3+3)
+console.log("abhshekh")
+let name ="biahri"
+let age = 16
+let isLoggedIn = false 
+
+
+//datatypes
+// number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+//null => standlone value
+//undefined =>
+// symbol=> unique
+
+//object
+
+console.log(typeof age); //number 
+console.log(typeof undefined);//undefined
+console.log(typeof null); //object 
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof  (score));
+
+// let valueInNumber = Number(score);
+// console.log (typeof valueInNumber);
+// console.log(valueInNumber);
+
+// //"33" => 33 value we get 
+// //"33abc" => NaN (not a number )
+// //true=> 1 ; false => 0
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//1=>true; 0 => fa;se
+//"" => false 
+// "abhisehkh" => true
+
+let someNumber = 33
+let stringNumber = String(SomeNumber)
+ console.log(stringNumber);
