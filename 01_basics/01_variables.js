ff76dcb72e81c36f46e8d7a3addd54d5626cb28d
abhishekhbihari007 <128975284+abhishekhbihari007@users.noreply.github.com>
2025-07-07 02:18:00 +0000

    Your branch is up to date with 'origin/main'.

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail ="bihari07@gamil.com"
+var accountPassword = "12345"
+accountycity = "jaipur"
+
+//accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "2121212"
+accountCity = "delhi"
+
+
+/*prefer not use var 
+bcz of issue in block scope and functional scope*/
+
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
