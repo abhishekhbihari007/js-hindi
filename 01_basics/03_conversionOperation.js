d9f4ba4833e0178bddefe856609303854c4a0259
abhishekhbihari007 <128975284+abhishekhbihari007@users.noreply.github.com>
2025-07-10 01:39:14 +0000

    types of conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,6 +20,72 @@
 //"" => false 
 // "abhisehkh" => true
 
-let someNumber = 33
-let stringNumber = String(SomeNumber)
- console.log(stringNumber);
+// let someNumber = 33
+// let stringNumber = String(SomeNumber)
+//  console.log(stringNumber);
+
+
+ ///////////////////OPERATIONS////////////////////////////////////
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = "abhishekh"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log("1"+"2");
+// console.log("1" + 2 + 2); USE PARANTHESIS()TO 
+// console.log(1 + 2 + "2"); SHOW THAT WHICH OPERATIONS YOU REQUIRED MORE
+
+// console.log(+true);
+
+// let num1 , num2 , num3
+
+// num1 = num2 =num3 = 2 + 2
+
+// let gameCounter = 100
+// ++gameChanger;
+// console.log(gameChanger);
+
+//Link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+// console.log(2>1); (these are basic and best conversion)
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//console.log("2" > 1);(this will make you stuck bcz one side is string datatype and other side is another datat type)
+//console.log("02" > 1);
+
+//console.log(null == 0);(we have to avoid these types of conversion )
+//console.log(null > 0);(make you code clear)
+//console.log(null >= 0);(clear code is our main priority)
+
+//console.log(undefined==0); (double equal works differently)
+//console.log(undefined>=0); (symbol of greater/smaller works differently)
+
+
+
+
+
+
+
+
+
+
+
