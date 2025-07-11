69c2693e46fa6fc161274b2bbda3b1d69de52e76
abhishekhbihari007 <128975284+abhishekhbihari007@users.noreply.github.com>
2025-07-11 01:35:26 +0000

    trim

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -15,6 +15,15 @@ const gameName = new String('biharii')//anotherway to declare string
 // console.log(gameName.length)
 // console.log(gameName.toUpperCase());
 console.log(gameName.charAt(3)); //to know at which index which word is there
-console.log(gameName.charAt('t')); //to know that any word is at which position
+console.log(gameName.indexOf('t')); //to know that any word is at which position
 
+const newString = gameName.substring(0,5)//it will not give the negative value 
+console.log(newString);
+
+const anotherString = gameName.slice(-7, 4)
+console.log(anotherString);
+
+const newStringOne = "      bihari   "
+console.log(newStringOne);
+console.log(newStringOne.trim());//trim is used to remove extra spaces 
 
