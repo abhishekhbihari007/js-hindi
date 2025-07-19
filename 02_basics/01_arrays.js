dddf43734f829b607cf3e2059de4e6108ce4ad62
abhishekhbihari007 <128975284+abhishekhbihari007@users.noreply.github.com>
2025-07-19 02:26:18 +0000

    splice done

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -5,7 +5,7 @@
 const myArr = [0,1,2,3,4]
 const myHeros = ["spiderMan","shaktiMan"]
 
-const myArr2 = new Array(1, 2, 3, 4,)
+const myArr2 = new Array(1, 2, 3, 4,5)
 //console.log(myArr[1]);
 
 //Array Methods
@@ -35,4 +35,6 @@ console.log(myn1);
 console.log("B", myArr);
 
 const myn2 = myArr.splice(1, 3)
+console.log("C", myArr); 
 console.log(myn2);
+
