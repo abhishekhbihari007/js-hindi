9b969c5330733637b86d3a52126dcc5be4687c98
abhishekhbihari007 <128975284+abhishekhbihari007@users.noreply.github.com>
2025-07-19 02:07:50 +0000

    aa

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,22 @@
+//array - is a object ,storing collection of multiple items under a single variable
+//JavaScript arrays are resizable and can contain a mix of different data types
+//JavaArrays starts with zero indexing
+
+const myArr = [0,1,2,3,4]
+const myHeros = ["spiderMan","shaktiMan"]
+
+const myArr2 = new Array(1, 2, 3, 4,)
+//console.log(myArr[1]);
+
+//Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)// to add any element in first 
+// myArr.shift()
+
+
+console.log(myArr);
+
