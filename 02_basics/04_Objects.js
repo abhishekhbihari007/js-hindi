01e9e2ef1123337acfc9f1d5de7cc85b7edb1d98
abhishekhbihari007 <128975284+abhishekhbihari007@users.noreply.github.com>
2025-07-20 02:39:03 +0000

    jason(javascript notation)

--- 02_basics/04_Objects.js
+++ 02_basics/04_Objects.js
@@ -1,6 +1,4 @@
 
-
-
 const course = {
     coursename: "js in hindi",
     price: "999",
@@ -12,3 +10,18 @@ const {courseInstructor: instructor} = course
 
 //console.log(courseConstructor);
 console.log(instructor);
+
+//*******************API*********************/
+
+// {
+//     "name": "bihari"
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {},
+]
+//here not always you will get api's in the form of object , it will also in in the form of array 
